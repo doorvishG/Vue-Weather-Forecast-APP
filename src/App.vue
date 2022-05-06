dafa90fb01e343f2a2bc25d4bfda7e280b76d8d1
Doorvish Gujoo <dgujoo@gmail.com>
2022-05-06 21:13:42 +0400

    Weather forecast app built on Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,134 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <weatherComp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import weatherComp from './components/WeatherComp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    weatherComp
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+ @import url('https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+
+ $font: 'Nunito', sans-serif;
+
+ *{
+   padding: 0;
+   margin: 0;
+   box-sizing: border-box;
+ }
+ 
+ body{
+   background-color: #094D92;
+ }
+
+ .card{
+   background-color: #2CA58D;
+   margin: 0 auto;
+   margin-top: 4em;
+   width: 35vw;
+   height: 22vh;
+   border-top-right-radius: 20px;
+   border-top-left-radius: 20px;
+
+   h1{
+     font-family: $font;
+     font-weight: 700;
+     color: #fff;
+     text-align: center;
+     padding-top: 0.8em;
+   }
+
+   input{
+     background: #2CA58D;
+     border: 1px solid #fff;
+     padding: 0.5em;
+     border-radius: 5px;
+     width: 15vw;
+     margin-top: 1.1em;
+     margin-left: 28%;
+     transition: 0.3s ease-in-out;
+
+    &:focus{
+      border: 2px solid #fff;
+      outline: none;
+    }
+   }
+   
+    input[type=text]{
+      color: #fff;
+      font-family: $font;
+      font-size: 1rem;
+    }
+
+   ::placeholder{
+     font-family: $font;
+     color:#fff;
+     font-size: 1rem;
+   }
+ }
+
+ @media (max-width:468px) {
+   .card{
+     input[type=text]{
+     font-size: 0.7rem;
+   }
+   ::placeholder{
+     font-size: 0.7rem;
+   }
+   }
+   
+ }
+
+ .result{
+   background-color: #1C1018;
+   border-bottom-left-radius: 20px;
+   border-bottom-right-radius: 20px;
+   margin: 0 auto;
+   width: 35vw;
+   height: 35vh;
+   display: flex;
+   justify-content: center;
+
+   .location{
+     margin-top: 3em;
+     margin-left: -4.5em;
+        
+     h2{
+       font-family: $font;
+       font-weight: 600;
+       color: #fff;
+     }
+
+     h1{
+       font-family: $font;
+       font-weight: 200;
+       color: #fff;
+     }
+
+     p{
+       font-family: $font;
+       font-weight: 400;
+       color: #fff;
+       line-height: 2;
+     }
+   }
+
+   .image{
+     margin-top: 3em;
+     padding-left:2em;
+      img{
+        width: 80px;
+        height: 66px;
+        
+      }
+   }
+ }
 </style>
---
+++ src/components/WeatherComp.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="card">
+        <h1>Weather forecast</h1>
+        <input @keyup.enter="submit()" v-model="city" type="text" placeholder="Enter location eg Port-Louis" >
+    </div>
+    
+    <div class="result">
+        <transition name="slideleft">
+            <div class="location" v-if="show">
+                <h2>{{location}}</h2>
+                <p>{{condition}}</p>
+                <h1>{{maxtemp}}</h1>
+                <p>{{maxwind}}</p>
+            </div>
+       </transition>
+        
+       <transition name="slideright">
+            <div class="image">
+                <img :src=image alt="" style="display: none" onload="this.style.display=''">
+            </div>
+        </transition>
+    </div>
+</template>
+
+
+<script>
+
+import axios from "axios"
+
+const api = process.env.VUE_APP_WF_API;
+
+export default {
+
+    data(){
+        return{
+            show:true,
+            city: "",
+            location:"",
+            condition:"",
+            maxtemp:"",
+            maxwind:"",
+            image:""
+        }
+    },
+    
+    methods:{
+        
+        async submit(){
+            if (!this.city){
+                return
+            }
+            try{
+                const url = `http://api.weatherapi.com/v1/forecast.json?key=${api}&q=${this.city}&days=1&aqi=no&alerts=no`
+                axios
+                .get(url)
+                .then(response => {
+                    // console.log(response)
+                    this.location = response.data.location.name
+                    this.condition = response.data.forecast.forecastday[0].day.condition.text
+                    this.maxtemp = response.data.forecast.forecastday[0].day.maxtemp_c + '°C'
+                    this.maxwind = response.data.forecast.forecastday[0].day.maxwind_kph + ' Km/h'
+                    this.image =  response.data.forecast.forecastday[0].day.condition.icon
+                })
+                
+
+
+
+            } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          console.log("Server Error:", err)
+        } else if (err.request) {
+          // client never received a response, or request never left
+          console.log("Network Error:", err)
+        } else {
+          console.log("Client Error:", err)
+        }
+            
+        }
+    }
+    }
+}
+</script>
+
+<style>
+    .slideleft-enter-from{
+        opacity: 0;
+        transform: translateX(-160px);
+    }
+
+    .slideleft-enter-to{
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .slideleft-enter-active{
+        transition: all 0.2s ease;
+    }
+</style>
